1d91c19df0518eb05b7ba81f2f44fe1078bd4f79
Max Serebryanskiy <maxim.serebryanskiy@icloud.com>
2016-09-14 15:29:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# My Finance [![Build Status](https://travis-ci.org/MaxMoto1702/finance.svg?branch=master)](https://travis-ci.org/MaxMoto1702/finance)
---
+++ grails-app/assets/javascripts/finance/finance.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /finance/core/finance.core
+//= require /finance/index/finance.index
+
+angular.module("finance", [
+        "finance.core",
+        "finance.index"
+    ]);
